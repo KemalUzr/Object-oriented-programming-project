b73712e1c9112f09cd6e227fa2e2c2cd5f13bd43
AliKemalUzar <alikemaluzar@gmail.com>
2020-05-24 23:18:57 +0200

    circus class

---
+++ src/Circus.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //vraagt alle informatie op circus.
+        Circus circus = new Circus();
+        circus.getAlles();
+
+        //vraagt informatie op van wat de voedingschema per kooi is met de bijhoorende dier.
+//        voedingSchema voedingSchema = new voedingSchema();
+//        voedingSchema.welkeVoeding();
+
+        //vraag afmetingen van "kleinKooi".
+//        Kooi kooi = new kleinKooi();
+//        System.out.println("klein kooi: "+kooi.lengteCM() +"cm lang, "+kooi.breedteCM()+"cm breed en "+ kooi.lengteCM()+"cm hoog");
+
+        //haalt personeels gegevens van de kantoorwerknemer.
+//        Personeel personeel = new Kantoorwerknemer();
+//        personeel.personeelsBestand();
+
+        //haalt gegevens van de Buffalo.
+//        Dier dier = new Buffalo();
+//        dier.dierGegevens();
+    }
+}
